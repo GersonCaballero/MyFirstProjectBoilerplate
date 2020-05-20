e6aaab24f26ad6f6ad1553b5cf237e32be1a2312
GersonCaballero <51335862+GersonCaballero@users.noreply.github.com>
2020-05-19 21:27:55 -0600

    new changes

---
+++ 5.4.0/src/MyFirstProjectBoilerPlate.Core/Employees/Employee.cs
@@ -0,0 +1,12 @@
+﻿using Abp.Domain.Entities;
+
+namespace MyFirstProjectBoilerPlate.Employees
+{
+    public class Employee : Entity
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Tel { get; set; }
+        public Department Department { get; set; }
+    }
+}
